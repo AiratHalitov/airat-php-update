b3c8b5a8cb3192fb2ad23512e8d1496e4da6685b
AiratHalitov <airat.halitov@gmail.com>
2020-12-16 01:35:51 +0500

    Update script, fix bugs

--- airat-php-update.sh
+++ airat-php-update.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 PHPOLD=7.4
-PHPNEW=7.3
+PHPNEW=8.0
 
-dpkg -l | grep php$PHPOLD | awk '{print $2;}' > php-$PHPOLD.txt
-cp php-$PHPOLD.txt php-$PHPNEW.txt
+echo -e "Current PHP version is: $(php -v)\n"
+dpkg -l | grep php$PHPOLD | awk '{print $2;}' > php$PHPOLD.txt
+cp php$PHPOLD.txt php$PHPNEW.txt
 
-sed "s/$PHPOLD/$PHPNEW/g" -i php-$PHPNEW.txt
+sed "s/$PHPOLD/$PHPNEW/g" -i php$PHPNEW.txt
 
-sudo apt install `cat php-$PHPNEW`
+sudo apt install `cat php$PHPNEW.txt`
 
 sudo apt remove libapache2-mod-php$PHPOLD
 sudo service apache2 restart
 
 sudo a2enmod php$PHPNEW
 sudo service apache2 restart
+sudo service mysql restart
 
 sudo apt update
-sudo apt remove `cat php-$PHPOLD.txt`
+sudo apt remove `cat php$PHPOLD.txt`
 # or with cleaning:
-# sudo apt purge `cat php-$PHPOLD.txt`
+#sudo apt purge `cat php$PHPOLD.txt`
+sudo apt update
+
+echo -e "\nCurrent PHP version is: $(php -v)\nDone!"
