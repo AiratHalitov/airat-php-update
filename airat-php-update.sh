7780039d1fcd6265b5cd8a6ef8096b0de4cd47bf
AiratHalitov <airat.halitov@gmail.com>
2021-02-09 03:54:11 +0500

    remove -> purge

--- airat-php-update.sh
+++ airat-php-update.sh
@@ -18,7 +18,7 @@ if [[ "$PHPOLD" != "$PHPNEW" ]]; then
 
     sudo apt install $(cat php$PHPNEW.txt)
 
-    sudo apt remove libapache2-mod-php$PHPOLD
+    sudo apt purge libapache2-mod-php$PHPOLD
     sudo service apache2 restart
 
     sudo a2enmod php$PHPNEW
@@ -26,9 +26,7 @@ if [[ "$PHPOLD" != "$PHPNEW" ]]; then
     sudo service mysql restart
 
     sudo apt update
-    sudo apt remove $(cat php$PHPOLD.txt)
-    # or with cleaning:
-    #sudo apt purge $(cat php$PHPOLD.txt)
+    sudo apt purge $(cat php$PHPOLD.txt)
     sudo apt update && sudo apt clean
 
     echo -e "\nCurrent PHP version is: $(php -v)\nDone!"
